de0a069401024523f17107dda9ee69964894650b
Brynn Rittenhouse <brynnritt@ksu.edu>
2020-02-07 14:49:07 -0600

    commit

---
+++ Data/Entree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    class abstract class Entree
+    {
+        public virtual Size Size { get; set; }
+
+    }
+}
