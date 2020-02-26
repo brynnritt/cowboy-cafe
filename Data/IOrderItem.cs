3b7ddd4f23cd2129fd711627a835560ecc3acc6d
Brynn Rittenhouse <brynnritt@ksu.edu>
2020-02-26 15:05:29 -0600

    Add OrderTest

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A base class representing a side
+    /// </summary>
+    public interface IOrderItem
+    {
+        public double Price { get; set; }
+
+        public List<string> SpecialIntructions { get; set; }
+    }
+}
---
+++ Data/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class Order : IOrderItem
+    {
+        public double Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<string> SpecialIntructions { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
